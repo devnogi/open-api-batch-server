5ed10e559f32278fd750596fd04779abd7e0186a
sh.lee <sh.lee@promptech.co.kr>
2025-04-23 10:52:10 +0900

    fix: change flyway sql v2 to lower case table name

--- src/main/resources/db/migration/V2__add_itemcategory_and_register_date_to_auction_history.sql
+++ src/main/resources/db/migration/V2__add_itemcategory_and_register_date_to_auction_history.sql
@@ -1,6 +1,6 @@
 #
-ALTER TABLE AUCTION_HISTORY ADD item_sub_category VARCHAR(25) NOT NULL COMMENT '아이템 하위 카테고리';
-ALTER TABLE AUCTION_HISTORY ADD item_top_category VARCHAR(25) NOT NULL COMMENT '아이템 상위 카테고리';
+ALTER TABLE auction_history ADD item_sub_category VARCHAR(25) NOT NULL COMMENT '아이템 하위 카테고리';
+ALTER TABLE auction_history ADD item_top_category VARCHAR(25) NOT NULL COMMENT '아이템 상위 카테고리';
 
 #
-ALTER TABLE AUCTION_HISTORY ADD date_register DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '해당 경매장 거래 내역을 수집한 시각';
+ALTER TABLE auction_history ADD date_register DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '해당 경매장 거래 내역을 수집한 시각';
