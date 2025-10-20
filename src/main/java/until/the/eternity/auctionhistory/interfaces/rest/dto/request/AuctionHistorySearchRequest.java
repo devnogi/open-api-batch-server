b2eab16d010e5203f4b796d89d61f40b5cbe8aec
Sanghyun Yi <njsharry32@gmail.com>
2025-10-21 00:23:55 +0900

    feat: auction history search에서 price request dto 제거

--- src/main/java/until/the/eternity/auctionhistory/interfaces/rest/dto/request/AuctionHistorySearchRequest.java
+++ src/main/java/until/the/eternity/auctionhistory/interfaces/rest/dto/request/AuctionHistorySearchRequest.java
@@ -7,6 +7,6 @@ public record AuctionHistorySearchRequest(
         @Schema(description = "아이템 이름 (like 검색)", example = "페러시우스 타이탄 블레이드") String itemName,
         @Schema(description = "대분류 카테고리", example = "근거리 장비") String itemTopCategory,
         @Schema(description = "소분류 카테고리", example = "검") String itemSubCategory,
+        // TODO: 거래 가격, 거래 일자를 범위 검색으로 변경, 옵션은 별도의 RequestDTO 구현git
         @Schema(description = "거래 가격", example = "10000000") String auction_price_per_unit,
-        @Schema(description = "거래 일자", example = "검") String date_auction_buy,
-        @Schema(description = "거래 금액") PriceSearchRequest priceSearchRequest) {}
+        @Schema(description = "거래 일자", example = "검") String date_auction_buy) {}
