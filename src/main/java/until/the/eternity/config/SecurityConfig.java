dff8bec2d785e83141e0a1eb5704e31179c928fc
dev-ant <njsharry32@gmail.com>
2025-12-05 12:27:49 +0900

    etc: security config에 todo 추가

--- src/main/java/until/the/eternity/config/SecurityConfig.java
+++ src/main/java/until/the/eternity/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
                                                 "/swagger-ui/**", "/v3/api-docs/**", "/docs/**")
                                         .permitAll()
                                         // API 엔드포인트는 공개
+                                        // TODO: API endpoint 정리 후 matcher 수정
+                                        // TODO: 권한 관련 기능 개발 완료 후 hasRole 추가
                                         .requestMatchers("/api/**", "/auction-history/**")
                                         .permitAll()
                                         // 나머지 요청은 인증 필요
