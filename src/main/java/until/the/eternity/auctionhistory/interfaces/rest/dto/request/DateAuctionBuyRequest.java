a8d6ca3add9154280976f5c30db7aeaa8de1fe5d
dev-ant <njsharry32@gmail.com>
2026-02-19 12:49:04 +0900

    feat: 경매장 거래 내역 검색 일자 sample 값 수정

--- src/main/java/until/the/eternity/auctionhistory/interfaces/rest/dto/request/DateAuctionBuyRequest.java
+++ src/main/java/until/the/eternity/auctionhistory/interfaces/rest/dto/request/DateAuctionBuyRequest.java
@@ -4,5 +4,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "거래 일자 조건")
 public record DateAuctionBuyRequest(
-        @Schema(description = "거래 일자 시작 범위", example = "2025-01-01") String dateAuctionBuyFrom,
-        @Schema(description = "거래 일자 종료 범위", example = "2025-12-31") String dateAuctionBuyTo) {}
+        @Schema(description = "거래 일자 시작 범위", example = "2026-02-01") String dateAuctionBuyFrom,
+        @Schema(description = "거래 일자 종료 범위", example = "2026-02-11") String dateAuctionBuyTo) {}
